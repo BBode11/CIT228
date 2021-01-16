c6ad0466a6c1a484d5aaf1cdefbc9d347e9f4bcb
Brandon Bode <brandonbode25@yahoo.com>
2021-01-16 10:05:32 -0500

    Add project files.

---
+++ Lesson1/stringFun.py
@@ -0,0 +1,46 @@
+print("------------------------------")
+print("Exercise 1")
+name = "Brandon Bode"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("My first and last intial are: ", name[0].upper())
+
+print("------------------------------")
+print("Exercise 2")
+noun1 = "turkey"
+noun2 = "wolf"
+adj = "full"
+verb = "ate"
+ending = "and is now"
+sentence1 = "The " + noun2 + " " + verb + " the " + noun1 + " " + ending + " " + adj
+sentence2 = "The " + noun1 + " " + verb + " the " + noun2 + " " + ending + " " + adj
+print(sentence1.lower())
+print(sentence2.upper())
+
+print("------------------------------")
+print("Exercise 3" + "\n")
+sentence3 = "I've only had two rules."
+sentence4 = "Do all you can and do it the best you can."
+sentence5 = "It's the only way you get that"
+sentence6 = "feeling of accomplishing something."
+signOff = "-Colonel Sanders-"
+print(sentence3.lower())
+print("\t\t" + sentence4.upper())
+print(sentence5.lower() + " " + sentence6.upper() + "\n")
+print("\t\t\t" + signOff.upper())
+
+
+print("------------------------------")
+print("Exercise 4" + "\n")
+color1 = "Orange"
+color2 = "Blue"
+color3 = "Pink"
+food1 = "Pie"
+food2 = "Ice Cream"
+food3 = "Chips"
+print("Colors" + "\t\t" + "Food")
+print("------" + "\t\t" + "----" + "\n")
+print(color1 + "\t\t" + food1 + "\n")
+print(color2 + "\t\t" + food2 + "\n")
+print(color3 + "\t\t" + food3 + "\n")
